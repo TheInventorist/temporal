208464db351c2133de5bcaf93c617634513e46e7
Vincent <vdepassier@outlook.com>
2022-11-08 17:20:43 -0300

    QUE TANTO

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# temporal
-this is a temporal repository
+1) Create project
+2) Create virtual environment with;
+    - python3 -m venv venv
+3) Activate virtual environment;
+    - .\venv\Scripts\Activate
+4) Intall dependecies with a "requirements.txt" file;
+    - pip install -r .\requirements.txt
+5) Code app starting from services
+6) build sh file
+7) build docker
+8) run docker
+9) upload somewhere
+10) conquer the world
---
+++ Services/main.py
@@ -0,0 +1,20 @@
+import uvicorn
+from fastapi import FastAPI
+import api_data
+from Models.Character_Model import CharacterModel as model
+
+app = FastAPI()
+
+@app.get("/")
+def index():
+    return {
+        "message" : "Hello World!"
+    }
+
+@app.get("/character/{id}", response_model=model)
+async def character_getter(id:int):
+    character = await api_data.get_characterById(id)
+    return character.dict()
+
+if __name__ == "__main__":
+    uvicorn.run(app)
